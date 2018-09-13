e16bea3e2c88b47a0591da99238935e0e603ff3f
Ilya Kalchenko <ilya.kalchenko@nixsolutions.com>
2018-09-13 12:35:52 +0300

    add reset password feature

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, UserListView, ActivateUserView
+from .views import SignUpView, UserListView, ActivateUserView, ResetPasswordView, ResetPasswordRequestView
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
@@ -9,13 +9,17 @@ router = routers.SimpleRouter()
 
 app_name = 'users'
 urlpatterns = [
-    path('signup', SignUpView.as_view(), name='sign-up'),
+    path('auth/signup', SignUpView.as_view(), name='sign-up'),
     path('users', UserListView.as_view(), name='user-list'),
-    path('login', obtain_jwt_token),
-    path('activate/<str:uidb64>/<str:token>',
+    path('auth/login', obtain_jwt_token),
+    path('auth/verify', verify_jwt_token),
+    path('auth/refresh', verify_jwt_token),
+    path('auth/activate/<str:uidb64>/<str:token>',
          ActivateUserView.as_view(),
          name='activate'),
-    # path('reset-password/<str:uidb64>/<str:token>',
-    #      views.ResetPasswordView.as_view(),
-    #      name='reset-password'),
+    path('auth/reset-password', ResetPasswordRequestView.as_view(),
+         name='reset-password-request'),
+    path('auth/reset-password/<str:uidb64>/<str:token>',
+         ResetPasswordView.as_view(),
+         name='reset-password'),
 ] + router.urls
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import generics, status, views
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .serializers import UserSerializer
 from .tokens import account_activation_token as aat
@@ -13,6 +13,7 @@ from .utils import send_confirmation_email
 
 
 class SignUpView(views.APIView):
+    permission_classes = (AllowAny,)
     serializer_class = UserSerializer
 
     def post(self, request, *args, **kwargs):
@@ -36,6 +37,8 @@ class UserListView(generics.ListAPIView):
 
 
 class ActivateUserView(views.APIView):
+    permission_classes = (AllowAny,)
+
     def get(self, request, *args, **kwargs):
         try:
             uid = force_text(urlsafe_base64_decode(kwargs['uidb64']))
@@ -45,5 +48,36 @@ class ActivateUserView(views.APIView):
         if user and aat.check_token(user, kwargs['token']):
             user.is_active = True
             user.save()
-            return Response({'status': 'succes'}, status.HTTP_200_OK)
+            return Response({'status': 'success'}, status.HTTP_200_OK)
         return Response({'status': 'fail'}, status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPasswordView(views.APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, *args, **kwargs):
+        try:
+            uid = force_text(urlsafe_base64_decode(kwargs['uidb64']))
+            user = User.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+            user = None
+        if user and prt.check_token(user, kwargs['token']):
+            user.set_password(self.request.POST['new_password'])
+            user.save()
+            return Response({'status': 'success'}, status=status.HTTP_200_OK)
+        return Response({'status': 'fail'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPasswordRequestView(views.APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, *args, **kwargs):
+        email = self.request.POST.get('email')
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            pass
+        if user:
+            send_confirmation_email(
+                self.request, user, reset_password=True)
+        return Response({'status': 'success'}, status.HTTP_200_OK)
